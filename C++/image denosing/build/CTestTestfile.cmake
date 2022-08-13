351793fbed1a59ee65b3df800bfb1ecf1314e29e
github_username <email_address>
2022-08-13 18:28:22 +0430

    denoising

---
+++ C++/image denosing/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/KAMBAKHSH/Desktop/Computer-Vision-projects/C++/image denosing
+# Build directory: C:/Users/KAMBAKHSH/Desktop/Computer-Vision-projects/C++/image denosing/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- README.md
+++
@@ -1,3 +0,0 @@
-# Computer-Vision-C++ && Python
-This repository contains some of my little side projects that I've been working on. 
-All of the including projects use only the traditional algorithms of computer vision and image processing.
