404f64a07a18d6297f2d43a745bdb64ba72e5ac2
Kambakhsh Eskandari <kambakhsh.eskandari1@gmail.com>
2023-03-27 13:09:30 +0430

    delete

--- C++/image denosing/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/KAMBAKHSH/Desktop/Computer-Vision-projects/C++/image denosing
-# Build directory: C:/Users/KAMBAKHSH/Desktop/Computer-Vision-projects/C++/image denosing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
